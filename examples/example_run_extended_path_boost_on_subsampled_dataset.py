6b1cfd5817ee8e65907b9cc58d4b5f6b86b06f88
Claudio <spaziotemporale.com@gmail.com>
2025-06-30 18:10:56 +0200

    improved naming when saving plots

--- examples/example_run_extended_path_boost_on_subsampled_dataset.py
+++ examples/example_run_extended_path_boost_on_subsampled_dataset.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(nx_graphs, y, test_size=0.2, random_state=42)
 
     # Initialize the PathBoost model
-    path_boost = PathBoost(n_iter=10000, max_path_length=6, learning_rate=0.02, n_of_cores=4, verbose=True,
+    path_boost = PathBoost(n_iter=10, max_path_length=6, learning_rate=0.02, n_of_cores=4, verbose=True,
                            parameters_variable_importance=parameters_variable_importance)
 
     # Define anchor nodes labels
